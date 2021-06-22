0be228f9a4209f3b9c917e092ba2c0cbae2a3e2f
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-22 18:27:08 -0300

    teste

---
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-photo-board',
+  templateUrl: './photo-board.component.html',
+  styleUrls: ['./photo-board.component.scss']
+})
+export class PhotoBoardComponent{
+
+}
